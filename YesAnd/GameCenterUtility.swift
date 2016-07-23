dec55d11929292855be5bab9af970bbd8c6c27a9
Husein Kareem <huseinkareem@gmail.com>
2016-07-23 14:39:52 -0500

    setup gamecenter authentication

---
+++ YesAnd/GameCenterUtility.swift
@@ -0,0 +1,26 @@
+//
+//  GameCenterUtility.swift
+//  YesAnd
+//
+//  Created by Husein Kareem on 7/23/16.
+//  Copyright © 2016 Yes, And, LLC. All rights reserved.
+//
+
+import UIKit
+import GameKit
+
+class GameCenterUtility {
+    
+    func authenticateLocalPlayer(viewController: UIViewController) {
+        let localPlayer = GKLocalPlayer.localPlayer()
+        localPlayer.authenticateHandler = {(gameCenterViewController: UIViewController?, error: NSError?) -> Void in
+            guard let gameCenterViewController = gameCenterViewController else {
+                return
+            }
+            
+            viewController.presentViewController(gameCenterViewController,
+                                                 animated: true,
+                                                 completion: nil)
+        }
+    }
+}
